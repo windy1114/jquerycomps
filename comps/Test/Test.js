7f7580098dfffa7e2aa420731c54b446708ff3fa
suchesqiu <x@btbtd.org>
2013-08-17 22:19:05 +0800

    sync master @20130817

--- comps/Test/Test.js
+++ comps/Test/Test.js
@@ -1,1 +1,1 @@
-(function(b){!window.JC&&(window.JC={log:function(){}});window.ZINDEX_COUNT=window.ZINDEX_COUNT||50001;window.Test=JC.Test=a;function a(c){}}(jQuery));
+(function(b){window.Test=JC.Test=a;function a(c){}}(jQuery));
