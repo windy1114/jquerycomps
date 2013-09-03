e1d3bac9fffb31680e757c1069ed4e274af3d29b
suchesqiu <x@btbtd.org>
2013-09-03 23:50:10 +0800

    sync master @20130903

--- tools/merge.comps.sh
+++ tools/merge.comps.sh
@@ -3,11 +3,6 @@ cd ..
 dir=$PWD; 
 cd $cur_dir;
 
-
-sh -c "sh generate_api_docs.sh";
-
-sleep 3s
-
 echo "sleep done";
 
 sh -c "node nodejs_merge.js";
@@ -15,5 +10,16 @@ sh -c "cd $dir/comps/Calendar && node nodejs_merge.js";
 sh -c "cd $dir/comps/Panel && node nodejs_merge.js";
 sh -c "cd $dir/comps/Form && node nodejs_merge.js";
 sh -c "cd $dir/comps/LunarCalendar && node nodejs_merge.js";
+
+sleep 1s;
+
+sh -c "cd $dir && tar -cvpzf tmp.tgz common.js JC.js"
+rm $dir/common.js
+rm $dir/JC.js
+
+sh -c "cd $cur_dir && sh generate_api_docs.sh";
+
+sh -c "cd $dir && tar -xvpzf tmp.tgz -C .";
+rm $dir/tmp.tgz
 
 echo $dir;
